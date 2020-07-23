436c109d56715ce29bb60913deaccdb981bed21a
Ethan Muller <ethanbmuller@gmail.com>
2020-07-23 12:20:01 -0400

    scrolly header state persists across page loads

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-
 const IndexPage = () => (
-  <Layout>
+  <>
     <div>im the index</div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-  </Layout>
+  </>
 )
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-
 const SecondPage = () => (
-  <Layout>
+  <>
     <div>hi</div>
     <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  </>
 )
 
 export default SecondPage
